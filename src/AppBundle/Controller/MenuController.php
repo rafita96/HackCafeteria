7190dfca32a5fee073fe8e2ed458047ede2b2596
rafita96 <rafael.peralta.blanco@gmail.com>
2017-10-07 08:20:06 -0700

    Servicios de tortas y sanwish

--- src/AppBundle/Controller/MenuController.php
+++ src/AppBundle/Controller/MenuController.php
@@ -4,19 +4,42 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class MenuController extends Controller
 {
 	/**
-     * @Route("/{platillo}", name="menu")
+     * @Route("/tortas", name="tortas")
      */
-    public function menuAction(Request $request, $platillo)
+    public function tortasAction(Request $request)
     {
+    	$tortas = array("Jamón" => 55, 
+            "Lomo" => 60, 
+            "Carne" => 65,
+            "Desebrada" => 65,
+            "Chorizo" => 50);
 
         // replace this example code with whatever you need
-        return $this->render('menu/index.html.twig', 
+        return $this->render('menu/tortas.html.twig', 
             array(
-                "hola" => $platillo,
+                "tortas" => $tortas,
+            ));
+    }
+
+    /**
+     * @Route("/sanwish", name="sanwish")
+     */
+    public function sanwishesAction(Request $request)
+    {
+    	$sanwishes = array("Jamon" => 20,
+            "Pollo" => 22,
+            "Triple" => 25,
+            "Frijoles" => 15);
+
+        // replace this example code with whatever you need
+        return $this->render('menu/tortas.html.twig', 
+            array(
+                "tortas" => $sanwishes,
             ));
     }
 }
